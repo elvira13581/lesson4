6695c1e59c4e59bbf0b784f4dfee1366bbe5b7dd
Elvira Manolova <e.manolova@web.de>
2021-06-17 15:33:08 +0200

    Urok

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment, Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Form from "./components/Form";
+import TodoList from './components/TodoList.jsx'
+
+class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state={
+      title: 'My ToDo List',
+      todos: [{id:1, title: 'Hello', completed: false}]
+    }
+  }
+
+  addNewToDo = (item) => {
+    this.setState(
+      {...this.state, 
+        todos: [...this.state.todos, {id: Date.now(), title: item, completed: false}]
+      }
+    )
+  }
+
+  changeCompleteProp = (id) => {
+    const _todos = [...this.state.todos]
+    const idx = _todos.findIndex(t=>t.id===id)
+    if (idx===-1) return null
+    _todos[idx].completed = !_todos[idx].completed
+    this.setState({...this.state, todos: _todos})
+  }
+
+  deleteItem = (id) => {
+    const _todos = [...this.state.todos]
+    const idx = _todos.findIndex(t=>t.id===id)
+    if (idx===-1) return null
+    _todos.splice(idx, 1) 
+    this.setState({...this.state, todos: _todos})
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <h1 className="text-center mt-3">{this.state.title}</h1>
+        <section id="form">
+          <Form onAdd={this.addNewToDo} />
+        </section>
+        <section id="list">
+          <TodoList 
+            todos={this.state.todos} 
+            onComplete={this.changeCompleteProp} 
+            onDelete={this.deleteItem}  
+          />
+        </section>
+      </Fragment>
+    );
+  }
 }
 
 export default App;
